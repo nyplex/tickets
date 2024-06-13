9618bdeabcc01005fb4bad1ca90b7cc878b9572d
Alex Nypels <nypelsa@gmail.com>
2024-06-13 22:22:55 +0100

    Adding Routes, Tests & Logic to the Orders Service.

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -7,10 +7,10 @@ import {
   errorHandler,
   currentUser,
 } from "@an-tickets-dev/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 
@@ -27,10 +27,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
